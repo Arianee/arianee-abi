2b263a863482c67c7722a91e12255c631b5dff0a
Stefdelec <s.delecroix@gmail.com>
2019-10-10 12:53:36 +0200

    chore(ci): abi are store here and build generate minified files

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
-import { Aria } from "./@types/Aria";
+import { Aria } from "./types/Aria";
 
-import { ArianeeCreditHistory } from "./@types/ArianeeCreditHistory";
-import { ArianeeIdentity } from "./@types/ArianeeIdentity";
-import { ArianeeSmartAsset } from "./@types/ArianeeSmartAsset";
-import { ArianeeStaking } from "./@types/ArianeeStaking";
-import { ArianeeStore } from "./@types/ArianeeStore";
-import { ArianeeWhitelist } from "./@types/ArianeeWhitelist";
-import {ArianeeEvent} from './@types/ArianeeEvent';
+import { ArianeeCreditHistory } from "./types/ArianeeCreditHistory";
+import { ArianeeIdentity } from "./types/ArianeeIdentity";
+import { ArianeeSmartAsset } from "./types/ArianeeSmartAsset";
+import { ArianeeStaking } from "./types/ArianeeStaking";
+import { ArianeeStore } from "./types/ArianeeStore";
+import { ArianeeWhitelist } from "./types/ArianeeWhitelist";
+import {ArianeeEvent} from './types/ArianeeEvent';
 
 export {
   Aria,
---
+++ minify.js
@@ -0,0 +1,39 @@
+const UglifyJS = require("uglify-js");
+const fs = require('fs');
+var path = require('path')
+
+const testFolder = './abi/';
+
+const dir={
+  js:testFolder+'js/',
+  json:testFolder+'json/'
+}
+
+if (!fs.existsSync(dir.js)) {
+  fs.mkdirSync(dir.js);
+};
+
+if (!fs.existsSync( dir.json)) {
+  fs.mkdirSync(dir.json);
+};
+
+
+const minifiedFiles = [];
+fs.readdirSync(testFolder).forEach(file => {
+  if (path.extname(file) === '.json') {
+    minifiedFiles.push(testFolder + file);
+    const content = fs.readFileSync(testFolder + file);
+    var minified = JSON.stringify(JSON.parse(content));
+    
+    // Write jsFile
+    const jsfileName = file.substr(0, file.lastIndexOf(".")) + ".js";
+    const jsContent="export default" + minified;
+    fs.writeFileSync(dir.js + jsfileName, jsContent);
+
+    // write json content
+    fs.writeFileSync(dir.json + file, minified);
+  }
+});
+
+console.log("FINISHED MINI-FYING")
+console.log(minifiedFiles);
