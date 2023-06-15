9b1f8ea7200c9cb763714a2c099b930ca952bb42
alexishecf <alexis@hecfeuille.fr>
2023-06-15 17:43:11 +0200

    feat: add ethers 6 types and factories

---
+++ index.ts
@@ -0,0 +1,29 @@
+import type { Aria } from "./types/Aria";
+
+import type { ArianeeCreditHistory } from "./types/ArianeeCreditHistory";
+import type { ArianeeIdentity } from "./types/ArianeeIdentity";
+import type { ArianeeSmartAsset } from "./types/ArianeeSmartAsset";
+import type { ArianeeStaking } from "./types/ArianeeStaking";
+import type { ArianeeStore } from "./types/ArianeeStore";
+import type { ArianeeWhitelist } from "./types/ArianeeWhitelist";
+import type { ArianeeEvent } from './types/ArianeeEvent';
+import type { ArianeeMessage } from './types/ArianeeMessage';
+import type { ArianeeUserAction } from './types/ArianeeUserAction';
+import type { ArianeeUpdate } from "./types/ArianeeUpdate";
+
+export type {
+  Aria,
+  ArianeeIdentity,
+  ArianeeSmartAsset,
+  ArianeeStaking,
+  ArianeeStore,
+  ArianeeWhitelist,
+  ArianeeCreditHistory,
+  ArianeeEvent,
+  ArianeeMessage,
+  ArianeeUserAction,
+  ArianeeUpdate
+};
+
+export * as ethers6_v1 from './ethers6/v1'
+export * as ethers6_v1_1 from './ethers6/v1_1'
