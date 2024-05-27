197255a842702c7a12025e23283be08525db9709
0xWhiteleaf <noe.comte@outlook.fr>
2024-05-27 15:00:32 +0200

    feat: add ArianeeIssuerProxy + ArianeeCreditNotePool and revert web3 dep to 1.8.1

--- ethers6/v1_1/factories/index.ts
+++ ethers6/v1_1/factories/index.ts
@@ -3,8 +3,10 @@
 /* eslint-disable */
 export { Aria__factory } from "./Aria__factory";
 export { ArianeeCreditHistory__factory } from "./ArianeeCreditHistory__factory";
+export { ArianeeCreditNotePool__factory } from "./ArianeeCreditNotePool__factory";
 export { ArianeeEvent__factory } from "./ArianeeEvent__factory";
 export { ArianeeIdentity__factory } from "./ArianeeIdentity__factory";
+export { ArianeeIssuerProxy__factory } from "./ArianeeIssuerProxy__factory";
 export { ArianeeLost__factory } from "./ArianeeLost__factory";
 export { ArianeeMessage__factory } from "./ArianeeMessage__factory";
 export { ArianeeSmartAsset__factory } from "./ArianeeSmartAsset__factory";
