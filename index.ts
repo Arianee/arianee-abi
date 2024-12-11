82d3d29985729652204e864432080a8ad5e7c70c
alexishecf <90714480+alexishecf@users.noreply.github.com>
2024-12-11 15:01:11 +0100

    feat: create ethers6 types and factories for v1.6

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export * as ethers6_v1 from './ethers6/v1'
-export * as ethers6_v1_1 from './ethers6/v1_1'
+export * as ethers6_v1 from './ethers6/v1';
+export * as ethers6_v1_5 from './ethers6/v1_5';
+export * as ethers6_v1_6 from './ethers6/v1_6';
